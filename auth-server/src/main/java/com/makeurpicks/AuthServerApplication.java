1529d241f8d4cb61847ca6b12145de991875529e
mojo.jojo.mj13@gmail.com <mojo.jojo.mj13@gmail.com>
2017-02-08 16:02:38 +0530

    myp-45 --> oauth db

--- auth-server/src/main/java/com/makeurpicks/AuthServerApplication.java
+++ auth-server/src/main/java/com/makeurpicks/AuthServerApplication.java
@@ -1,16 +1,24 @@
 package com.makeurpicks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,191 +26,225 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.makeurpicks.domain.Authority;
+import com.makeurpicks.domain.OAuthClientDetailsBuilder;
+import com.makeurpicks.domain.OAuthClientDetails;
+import com.makeurpicks.domain.Player;
+import com.makeurpicks.domain.PlayerBuilder;
+import com.makeurpicks.service.OAuthClientsService;
 import com.makeurpicks.service.PlayerService;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaRepositories
-//@EnableResourceServer
-public class AuthServerApplication extends WebMvcConfigurerAdapter {
+// @EnableResourceServer
+public class AuthServerApplication extends WebMvcConfigurerAdapter implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(AuthServerApplication.class);
-    
+
+	@Autowired
+	private PlayerService playerService;
+
+	@Autowired
+	private OAuthClientsService clientService;
+
+	@Value("${config.oauth2.ui-uri:http://localhost:8080/}")
+	private String ui;
+
+	@Value("${config.oauth2.admin-uri:http://localhost:9000/admin/}")
+	private String admin;
+
 	public static void main(String[] args) {
-        SpringApplication.run(AuthServerApplication.class, args);
-    }
-	
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
 	@Configuration
-    @EnableWebSecurity
-    @EnableGlobalMethodSecurity(prePostEnabled = true)
-    protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@EnableWebSecurity
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		@Autowired
-	    private PlayerService playerService;
-		
+		private PlayerService playerService;
+
 		@Autowired
 		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		    auth
-		    .userDetailsService(playerService)
-		    .passwordEncoder(passwordEncoder());
+			auth.userDetailsService(playerService).passwordEncoder(passwordEncoder());
 		}
-//		
+
 		@Bean
-	    public BCryptPasswordEncoder passwordEncoder() {
-	        return new BCryptPasswordEncoder();
-	    }
-	
-		
-//        @Override
-//        @Autowired // <-- This is crucial otherwise Spring Boot creates its own
-//        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//            log.info("Defining inMemoryAuthentication (2 users)");
-//            auth
-//                    .inMemoryAuthentication()
-//
-//                    .withUser("user").password("password")
-//                    .roles("USER")
-//
-//                    .and()
-//
-//                    .withUser("admin").password("password")
-//                    .roles("USER", "ADMIN")
-//            ;
-//        }
+		public BCryptPasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .formLogin()
+		// @Override
+		// @Autowired // <-- This is crucial otherwise Spring Boot creates its
+		// own
+		// protected void configure(AuthenticationManagerBuilder auth) throws
+		// Exception {
+		// log.info("Defining inMemoryAuthentication (2 users)");
+		// auth
+		// .inMemoryAuthentication()
+		//
+		// .withUser("user").password("password")
+		// .roles("USER")
+		//
+		// .and()
+		//
+		// .withUser("admin").password("password")
+		// .roles("USER", "ADMIN")
+		// ;
+		// }
 
-                    .and()
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.formLogin().and()
+					// disable http basic Authentication
+					.httpBasic().disable()
+					// disable anonymous
+					.anonymous().disable()
+					// authorize+ authenticate the requests
+					.authorizeRequests().anyRequest().authenticated();
+		}
 
-                    .httpBasic().disable()
-                    .anonymous().disable()
-                    .authorizeRequests().anyRequest().authenticated()
-            ;
-        }
 		@Override
 		public void configure(WebSecurity web) throws Exception {
+			// ignore all with patter /players/**
 			web.ignoring().antMatchers("/players/");
 		}
-        
-        
-    }
 
-    @Configuration
-    @EnableAuthorizationServer
-    protected static class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	}
 
-        @Value("${config.oauth2.privateKey}")
-        private String privateKey;
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-        @Value("${config.oauth2.publicKey}")
-        private String publicKey;
+		@Value("${config.oauth2.privateKey:private_default}")
+		private String privateKey;
 
-        @Autowired
-        private AuthenticationManager authenticationManager;
-        
-        @Value("${config.oauth2.ui-uri}")
-    	private String ui;
-    	
-        @Value("${config.oauth2.admin-uri}")
-    	private String admin;
+		@Value("${config.oauth2.publicKey:public_default}")
+		private String publicKey;
 
-        @Bean
-        public JwtAccessTokenConverter tokenEnhancer() {
-            log.info("Initializing JWT with public key:\n" + publicKey);
-            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-            converter.setSigningKey(privateKey);
-            converter.setVerifierKey(publicKey);
-            return converter;
-        }
+		@Autowired
+		private AuthenticationManager authenticationManager;
 
-        @Bean
-        public JwtTokenStore tokenStore() {
-            return new JwtTokenStore(tokenEnhancer());
-        }
+		@Autowired
+		private OAuthClientsService clientService;
 
-        /**
-         * Defines the security constraints on the token endpoints /oauth/token_key and /oauth/check_token
-         * Client credentials are required to access the endpoints
-         *
-         * @param oauthServer
-         * @throws Exception
-         */
-        @Override
-        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-            oauthServer
-                    .tokenKeyAccess("isAnonymous() || hasRole('ROLE_TRUSTED_CLIENT')") // permitAll()
-                    .checkTokenAccess("hasRole('TRUSTED_CLIENT')"); // isAuthenticated()
-        }
+		@Autowired
+		private Environment environment;
 
-        /**
-         * Defines the authorization and token endpoints and the token services
-         *
-         * @param endpoints
-         * @throws Exception
-         */
-        @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints
+		@Autowired
+		private DataSource dataSource;
 
-                    // Which authenticationManager should be used for the password grant
-                    // If not provided, ResourceOwnerPasswordTokenGranter is not configured
-                    .authenticationManager(authenticationManager)
+		@Bean
+		public JwtAccessTokenConverter tokenEnhancer() {
+			log.info("Initializing JWT with public key:\n" + publicKey);
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			if (Arrays.stream(environment.getActiveProfiles()).anyMatch(env -> (env.equalsIgnoreCase("test")))) {
+				converter.setSigningKey(privateKey);
+				// converter.setVerifierKey(publicKey);
+			} else {
+				converter.setSigningKey(privateKey);
+				converter.setVerifierKey(publicKey);
+			}
 
-                            // Use JwtTokenStore and our jwtAccessTokenConverter
-                    .tokenStore(tokenStore())
-                    .accessTokenConverter(tokenEnhancer())
-            ;
-        }
+			return converter;
+		}
 
-        @Override
-        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.inMemory()
+		@Bean
+		public JwtTokenStore tokenStore() {
+			return new JwtTokenStore(tokenEnhancer());
+		}
 
-                    // Confidential client where client secret can be kept safe (e.g. server side)
-                    .withClient("confidential").secret("secret")
-                    .authorizedGrantTypes("client_credentials", "authorization_code", "refresh_token")
-                    .scopes("read", "write")
-                    .redirectUris(admin).autoApprove(true)
+		/**
+		 * Defines the security constraints on the token endpoints
+		 * /oauth/token_key and /oauth/check_token Client credentials are
+		 * required to access the endpoints
+		 *
+		 * @param oauthServer
+		 * @throws Exception
+		 */
+		@Override
+		public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+			oauthServer.tokenKeyAccess("isAnonymous() || hasRole('ROLE_TRUSTED_CLIENT')") // permitAll()
+					.checkTokenAccess("hasRole('TRUSTED_CLIENT')"); // isAuthenticated()
 
-                    .and()
-                    
-                    .withClient("ui").secret("secret")
-                    .authorizedGrantTypes("client_credentials", "authorization_code", "refresh_token")
-                    .scopes("read", "write")
-                    .redirectUris(ui).autoApprove(true)
+			oauthServer.passwordEncoder(passwordEncoder());
+		}
 
-                    .and()
+		@Bean
+		public BCryptPasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
 
-                            // Public client where client secret is vulnerable (e.g. mobile apps, browsers)
-                    .withClient("public") // No secret!
-                    .authorizedGrantTypes("client_credentials", "implicit")
-                    .scopes("read")
-                    .redirectUris("http://localhost:9000/").autoApprove(true)
+		/**
+		 * Defines the authorization and token endpoints and the token services
+		 *
+		 * @param endpoints
+		 * @throws Exception
+		 */
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints
+					// Which authenticationManager should be used for the
+					// password grant
+					// If not provided, ResourceOwnerPasswordTokenGranter is not
+					// configured
+					.authenticationManager(authenticationManager)
+					// Use JwtTokenStore and our jwtAccessTokenConverter
+					.tokenStore(tokenStore()).accessTokenConverter(tokenEnhancer());
+		}
 
-                    .and()
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			// looks for the clients in DB rather than in memory
+			clients.jdbc(dataSource).passwordEncoder(passwordEncoder()).clients(clientService);
+		}
 
-                            // Trusted client: similar to confidential client but also allowed to handle user password
-                    .withClient("trusted").secret("secret")
-                    .authorities("ROLE_TRUSTED_CLIENT")
-                    .authorizedGrantTypes("client_credentials", "password", "authorization_code", "refresh_token")
-                    .scopes("read", "write")
-                    .redirectUris("http://localhost:9000/").autoApprove(true)
-            ;
-        }
+	}
 
-    }
+	@Override
+	public void run(String... args) throws Exception {
+		PlayerBuilder p = new PlayerBuilder("prithvish", "prithvish.mukherjee@techolution.com", "secret");
+		p.adAdmin();
+		Player player = p.build();
+		playerService.createPlayer(player);
+		//add clients to db
+		addClientsList();
+	}
+
+	/**
+	 * add List of clients data to DB
+	 */
+	private void addClientsList() {
+		List<OAuthClientDetails> clientList = new ArrayList<OAuthClientDetails>();
+		clientList.add(new OAuthClientDetailsBuilder("confidential", "secret", "ROLE_ADMIN",
+				"client_credentials,authorization_code,refresh_token", "read,write", admin + ",", "true").build());
+
+		clientList.add(new OAuthClientDetailsBuilder("ui", "secret", "ROLE_USER",
+				"client_credentials,authorization_code,refresh_token", "read,write", ui, "true").build());
+
+		clientList.add(new OAuthClientDetailsBuilder("public", "", "ROLE_ADMIN", "client_credentials,implicit", "read",
+				"http://localhost:9000/", "true").build());
+
+		clientList.add(new OAuthClientDetailsBuilder("trusted", "secret", "ROLE_TRUSTED_CLIENT",
+				"client_credentials,password,authorization_code,refresh_token", "read,write", "http://localhost:9000/",
+				"true").build());
+
+		clientService.createOAuthClientDetailsList(clientList);
+	}
+
 }
---
+++ auth-server/src/main/java/com/makeurpicks/constants/DAO_Constants.java
@@ -0,0 +1,10 @@
+package com.makeurpicks.constants;
+
+public class DAO_Constants {
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+	public static final String ROLE_USER = "ROLE_USER";
+
+	public static final String ROLE_TRUSTED_CLIENT = "ROLE_TRUSTED_CLIENT";
+
+}
